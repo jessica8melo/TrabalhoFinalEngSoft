d1a6a0fc1563fb48bd899223af8650baae222ece
jessicamelo <jessica8melo@gmail.com>
2026-05-27 16:25:32 -0300

    ✨ Funcionamento logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,6 +25,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: 'Logout realizado com sucesso'
+    redirect_to login_path, flash: { logout: "Logout realizado com sucesso!" }
   end
 end
