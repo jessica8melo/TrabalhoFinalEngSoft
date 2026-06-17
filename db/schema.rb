30790adfee3555986a8ab5a9b8239daf2453e40c
jessicamelo <jessica8melo@gmail.com>
2026-06-16 22:57:09 -0300

    🚑 fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_16_200000) do
   create_table "discentes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "curso"
@@ -45,20 +45,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
     t.index ["turma_id"], name: "index_docentes_on_turma_id"
   end
 
-  create_table "forms", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "destiny_type"
-    t.datetime "end_date"
-    t.datetime "start_date"
-    t.string "template_name"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "forms_turmas", id: false, force: :cascade do |t|
-    t.integer "form_id", null: false
-    t.integer "turma_id", null: false
-  end
-
   create_table "formularios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "deadline"
@@ -67,16 +53,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
     t.integer "turma_id", null: false
     t.datetime "updated_at", null: false
     t.index ["turma_id"], name: "index_formularios_on_turma_id"
-  end
-
-  create_table "pergunta", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.text "enunciado"
-    t.integer "formulario_id", null: false
-    t.boolean "obrigatoria"
-    t.string "tipo_pergunta"
-    t.datetime "updated_at", null: false
-    t.index ["formulario_id"], name: "index_pergunta_on_formulario_id"
   end
 
   create_table "perguntas", force: :cascade do |t|
@@ -89,18 +65,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
     t.index ["formulario_id"], name: "index_perguntas_on_formulario_id"
   end
 
-  create_table "resposta", force: :cascade do |t|
-    t.text "conteudo"
-    t.datetime "created_at", null: false
-    t.integer "formulario_id", null: false
-    t.integer "pergunta_id", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["formulario_id"], name: "index_resposta_on_formulario_id"
-    t.index ["pergunta_id"], name: "index_resposta_on_pergunta_id"
-    t.index ["user_id"], name: "index_resposta_on_user_id"
-  end
-
   create_table "respostas", force: :cascade do |t|
     t.text "conteudo"
     t.datetime "created_at", null: false
@@ -111,6 +75,27 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
     t.index ["formulario_id"], name: "index_respostas_on_formulario_id"
     t.index ["pergunta_id"], name: "index_respostas_on_pergunta_id"
     t.index ["user_id"], name: "index_respostas_on_user_id"
+  end
+
+  create_table "sigaa_logs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "message"
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_sigaa_logs_on_user_id"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "descricao"
+    t.string "nome", null: false
+    t.integer "parent_template_id"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "version", default: 1, null: false
+    t.index ["parent_template_id"], name: "index_templates_on_parent_template_id"
+    t.index ["user_id"], name: "index_templates_on_user_id"
   end
 
   create_table "turmas", force: :cascade do |t|
@@ -139,13 +124,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_14_123033) do
   add_foreign_key "discentes", "turmas"
   add_foreign_key "docentes", "turmas"
   add_foreign_key "formularios", "turmas"
-  add_foreign_key "pergunta", "formularios"
   add_foreign_key "perguntas", "formularios"
-  add_foreign_key "resposta", "formularios"
-  add_foreign_key "resposta", "perguntas"
-  add_foreign_key "resposta", "users"
   add_foreign_key "respostas", "formularios"
   add_foreign_key "respostas", "perguntas"
   add_foreign_key "respostas", "users"
+  add_foreign_key "sigaa_logs", "users"
+  add_foreign_key "templates", "templates", column: "parent_template_id"
+  add_foreign_key "templates", "users"
   add_foreign_key "turmas", "disciplinas"
 end
